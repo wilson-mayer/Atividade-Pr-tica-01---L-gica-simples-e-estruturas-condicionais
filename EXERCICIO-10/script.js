3d10e976ac9b689c11351f17964a4e1c413197cb
Wilson Ramos Mayer <wr.mayer@live.com>
2023-12-12 22:43:52 -0300

    Exercicio 10

---
+++ EXERCICIO-10/script.js
@@ -0,0 +1,10 @@
+// 10. Escreva um algoritmo que tenha como valores de entradas o nome
+//  e idade do usuário e imprima no terminal o nome, a idade e o ano
+//  de nascimento do usuário. Ex: “Nome: Pedro, Idade: 22 anos, nasceu
+//  em 2000”.
+//  OBS: Pegue o ano atual como base
+
+let nome = prompt('Digite seu nome');
+let idade = Number(prompt('Digite sua idade em anos'));
+
+console.log('Nome:', nome, 'Idade:', idade, "anos", 'nasceu em 2023');
